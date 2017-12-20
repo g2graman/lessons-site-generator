a4bf40f69f1145236ee9f0b6dc2795173c12c444
Francesco Gramano <fran.gramano@mail.utoronto.ca>
2017-12-20 01:38:59 -0500

    Styles non-interactive code blocks

--- src/pages/Module.js
+++ src/pages/Module.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { getRouteProps, Link } from 'react-static'
+import { Button } from 'rmwc/Button'
+
+import './module.css'
 
 export default getRouteProps(({ module }) => (
   <div>
-    <Link to="/">{'<'} Back</Link>
+    <Link to="/">
+      <Button stroked>Back to Modules</Button>
+    </Link>
     <br />
     <h3>{module.title}</h3>
     <p dangerouslySetInnerHTML={{ __html: module.body }} />
