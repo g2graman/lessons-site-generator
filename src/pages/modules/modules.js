a3c849398f883583b0083331a8c5aeefd49852d8
Francesco Gramano <fran.gramano@mail.utoronto.ca>
2018-01-21 09:58:35 -0500

    Adds working prototype of module lists generated from nestable content file structure

--- src/pages/modules/modules.js
+++ src/pages/modules/modules.js
@@ -1,66 +1,132 @@
 import React from "react";
 import { getRouteProps, Link } from "react-static";
-import { get } from "lodash";
+import { get, merge, capitalize } from "lodash";
 
 import Footer from "../../components/footer/footer";
 
-const convertFilePathToJSON = (filePath, acc = {}) => {
-  const [childDir, ...nestedPath] = filePath.split("/");
+const getModuleFromPath = (modules, modulePath) =>
+  modules.find(bridgeModule => bridgeModule.path === modulePath);
+
+const convertFilePathToJSON = (
+  modules,
+  filePath,
+  acc = {},
+  DELIMITER = "/"
+) => {
+  if (!filePath) {
+    return acc;
+  }
+
+  const [childDir, ...nestedPath] = filePath.split(DELIMITER);
 
   if (childDir) {
+    if (nestedPath.length > 1) {
+      return {
+        ...acc,
+        [childDir]: {
+          ...convertFilePathToJSON(
+            modules,
+            nestedPath.join(DELIMITER),
+            acc,
+            DELIMITER
+          )
+        }
+      };
+    }
+
+    // / nestedPath.length === <= 1
+
+    const fullPath = [childDir].concat(nestedPath).join(DELIMITER);
+    const remainingPath = nestedPath.join(DELIMITER);
+
+    const leaf = convertFilePathToJSON(
+      modules,
+      remainingPath,
+      get(acc, childDir, {}),
+      DELIMITER
+    );
+
     return {
-      [childDir]:
-        nestedPath.length > 1
-          ? {
-              ...convertFilePathToJSON(
-                nestedPath.join("/"),
-                get(acc, childDir, {})
-              )
-            }
-          : [...nestedPath, ...get(acc, [childDir].join("."), [])]
+      ...acc,
+      [childDir]: merge(
+        leaf,
+        nestedPath.length
+          ? { [remainingPath]: getModuleFromPath(modules, fullPath) } // nestedPath === 1
+          : getModuleFromPath(modules, fullPath) // nestedPath === 0
+      )
     };
   }
 
   return acc;
 };
 
-const getFileTree = relativeFilePaths =>
-  relativeFilePaths.reduce(
+const getFileTree = modules =>
+  modules.map(bridgeModule => bridgeModule.path).reduce(
     (acc, next) => ({
       ...acc,
-      ...convertFilePathToJSON(next, acc)
+      ...convertFilePathToJSON(modules, next, acc)
     }),
     {}
   );
 
-const getModuleLink = (bridgeModule, withPrefix = true) =>
-  (withPrefix ? ["modules"] : []).concat([bridgeModule.path]).join("/");
+const getModuleLink = (bridgeModule, withPrefix = true, DELIMITER = "/") =>
+  (withPrefix ? ["modules"] : []).concat([bridgeModule.path]).join(DELIMITER);
 
-const makeModuleLinks = modules => {
-  /* console.log(modules); */
+const makeModuleLink = bridgeModule => (
+  <li key={bridgeModule.id}>
+    <Link to={getModuleLink(bridgeModule)}>{bridgeModule.title}</Link>
+  </li>
+);
 
-  console.log(getFileTree(modules.map(bridgeModule => bridgeModule.path)));
-  /*
-    Object.entries(
-        getFileTree(modules.map(bridgeModule => bridgeModule.path))
-    ).reduce((acc, [key, value]) => {
-        // if ()
-        console.log(acc, key, value);
-        return acc;
-    }, {}); */
+const makeModuleLinks = modules => modules.map(makeModuleLink);
 
-  return modules.map(bridgeModule => (
-    <li key={bridgeModule.id}>
-      <Link to={getModuleLink(bridgeModule)}>{bridgeModule.title}</Link>
-    </li>
-  ));
+const makeModuleTree = rootModulesWithChildren =>
+  Object.entries(rootModulesWithChildren).map(([rootModuleKey, rootModule]) => {
+    if (rootModule.title) {
+      return makeModuleLink(rootModule);
+    }
+
+    return (
+      <li key={rootModuleKey}>
+        {capitalize(rootModuleKey)} <ul>{makeModuleTree(rootModule)}</ul>
+      </li>
+    );
+  });
+
+const getBarrenRootModules = modules =>
+  Object.entries(getFileTree(modules))
+    // eslint-disable-next-line no-unused-vars
+    .filter(([key, value]) => !!value.title) // TODO: match to whole interface (check for id, path, contents, data, etc)
+    // eslint-disable-next-line no-unused-vars
+    .map(([_, moduleMetadata]) => moduleMetadata); // only keep the module metadata
+
+const getRootModulesWithChildren = modules => {
+  const moduleTree = getFileTree(modules);
+
+  return merge(
+    // merge all the root modules into one object, forming a sub-object of the modules map
+    ...Object.entries(moduleTree)
+      // eslint-disable-next-line no-unused-vars
+      .filter(([key, value]) => !value.title) // TODO: match to whole interface (check for id, path, contents, data, etc)
+      // eslint-disable-next-line no-unused-vars
+      .map(([moduleKey, _]) => ({
+        [moduleKey]: get(moduleTree, moduleKey, {})
+      }))
+  );
 };
+
+const makeModuleList = modules => (
+  <ul>
+    {makeModuleLinks(getBarrenRootModules(modules))}
+    {makeModuleTree(getRootModulesWithChildren(modules))}
+  </ul>
+);
 
 export default getRouteProps(({ modules }) => (
   <div>
     <br />
     All Modules:
-    <ul>{makeModuleLinks(modules)}</ul>
+    {makeModuleList(modules)}
     <Footer />
   </div>
 ));
